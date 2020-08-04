3af11c90c544e25fce407b78bcde53f8d004ab2d
Jim X Lin <miljinx@gmail.com>
2020-08-03 22:32:36 -0700

    Set up authentication

--- app/db.py
+++ app/db.py
@@ -23,7 +23,6 @@ def init_db():
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
 
-
 @click.command('init-db')
 @with_appcontext
 def init_db_command():
@@ -34,4 +33,3 @@ def init_db_command():
 def init_app(app):
     app.teardown_appcontext(close_db)
     app.cli.add_command(init_db_command)
-
