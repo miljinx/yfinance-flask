a88ec11f9d03c97fc4482d1a98b1dc65a244ab1c
Jim X Lin <miljinx@gmail.com>
2020-08-22 16:02:21 -0700

    Prevent submit-button-spam with javascript

---
+++ app/static/script.js
@@ -0,0 +1,29 @@
+window.addEventListener('DOMContentLoaded', function(event) {
+  handleClickFind();
+  handleClickDownload();
+});
+
+function handleClickFind() {
+  var form = document.getElementById("find-form");
+  form.addEventListener("submit", function() {
+    var submitBtn = document.getElementById("find");
+    submitBtn.setAttribute("disabled", "");
+    submitBtn.setAttribute("value", "Finding");
+  });
+}
+
+// TODO use fetch api for callback functionality
+// see https://blog.logrocket.com/programmatic-file-downloads-in-the-browser-9a5186298d5c/
+function handleClickDownload() {
+  var form = document.getElementById("download-form");
+  var submitTimeout = 10;
+  form.addEventListener("submit", function() {
+    var submitBtn = document.getElementById("download");
+    submitBtn.setAttribute("disabled", "");
+    submitBtn.setAttribute("value", "Downloading...");
+    window.setTimeout(function() {
+      submitBtn.removeAttribute("disabled");
+      submitBtn.setAttribute("value", "Download");
+    }, 1000*submitTimeout);
+  });
+}
