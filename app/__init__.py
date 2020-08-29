045663e6c307f70c1941951aca188aae8375b21b
Jim X. Lin <miljinx@gmail.com>
2020-08-29 00:18:49 -0700

    Add python linting

--- app/__init__.py
+++ app/__init__.py
@@ -7,11 +7,11 @@ def create_app(test_config=None):
 
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
+        SECRET_KEY="dev",
     )
 
     if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile("config.py", silent=True)
     else:
         app.config.from_mapping(test_config)
 
@@ -21,7 +21,8 @@ def create_app(test_config=None):
         pass
 
     from . import dashboard
+
     app.register_blueprint(dashboard.bp)
-    app.add_url_rule('/', endpoint='index')
+    app.add_url_rule("/", endpoint="index")
 
     return app
--- app/dashboard.py
+++ app/dashboard.py
@@ -7,114 +7,119 @@ import humanize
 from flask import (
     Blueprint,
     flash,
-    get_flashed_messages,
-    g,
     session,
     redirect,
     render_template,
     request,
     url_for,
-    send_file)
+    send_file,
+)
 
 import yfinance as yf
 
-bp = Blueprint('dashboard', __name__)
+bp = Blueprint("dashboard", __name__)
 
 DOWNLOAD_INTERVAL_SECONDS = 10
 
 PERIOD_OPTIONS = (
-    ('1mo', '1 month'),
-    ('3mo', '3 months'),
-    ('6mo', '6 months'),
-    ('1y', '1 year'),
-    ('2y', '2 years'),
-    ('5y', '5 years'),
-    ('10y', '10 years'),
-    ('ytd', 'Year to Date'),
-    ('max', 'Max'),
+    ("1mo", "1 month"),
+    ("3mo", "3 months"),
+    ("6mo", "6 months"),
+    ("1y", "1 year"),
+    ("2y", "2 years"),
+    ("5y", "5 years"),
+    ("10y", "10 years"),
+    ("ytd", "Year to Date"),
+    ("max", "Max"),
 )
 
 INTERVAL_OPTIONS = (
-    ('1d', '1 day'),
-    ('5d', '5 days'),
-    ('1wk', '1 week'),
-    ('1mo', '1 month'),
-    ('3mo', '3 months'),
+    ("1d", "1 day"),
+    ("5d", "5 days"),
+    ("1wk", "1 week"),
+    ("1mo", "1 month"),
+    ("3mo", "3 months"),
 )
 
-@bp.route('/', methods=('GET', 'POST'))
+
+@bp.route("/", methods=("GET", "POST"))
 def index():
-    if request.method == 'POST':
-        ticker_symbol = request.form['ticker_symbol']
+    if request.method == "POST":
+        ticker_symbol = request.form["ticker_symbol"]
         retrieved_ticker_symbol = None
         try:
             yf_ticker = yf.Ticker(ticker_symbol)
-            retrieved_ticker_symbol = yf_ticker.info['symbol']
+            retrieved_ticker_symbol = yf_ticker.info["symbol"]
         except ValueError:
             flash(f"Ticker symbol '{ticker_symbol}' not found.")
 
         if retrieved_ticker_symbol:
             return redirect(
-                url_for(
-                    'dashboard.info',
-                    ticker_symbol=retrieved_ticker_symbol))
+                url_for("dashboard.info", ticker_symbol=retrieved_ticker_symbol)
+            )
 
-    return render_template('dashboard/index.html')
+    return render_template("dashboard/index.html")
 
 
-@bp.route('/info')
+@bp.route("/info")
 def info():
-    date_of_search = datetime.now().strftime('%B %-d, %Y')
-    ticker_symbol = request.args.get('ticker_symbol').upper()
+    date_of_search = datetime.now().strftime("%B %-d, %Y")
+    ticker_symbol = request.args.get("ticker_symbol").upper()
     # TODO cache ticker_info
     yf_ticker = yf.Ticker(ticker_symbol)
     ticker_info = yf_ticker.info
     details = {
-        'name': ticker_info['longName'],
-        'symbol': ticker_info['symbol'],
-        'industry': ticker_info['industry'],
-        'logo_url': ticker_info['logo_url'],
-        'web_url': ticker_info['website'],
-        'description': ticker_info['longBusinessSummary'],
-        'market_cap': humanize.intword(ticker_info['marketCap'])
-        if ticker_info['marketCap'] > 999999
-        else humanize.intcomma(ticker_info['marketCap']),
-        'open_price': humanize.intcomma(ticker_info['open']),
+        "name": ticker_info["longName"],
+        "symbol": ticker_info["symbol"],
+        "industry": ticker_info["industry"],
+        "logo_url": ticker_info["logo_url"],
+        "web_url": ticker_info["website"],
+        "description": ticker_info["longBusinessSummary"],
+        "market_cap": humanize.intword(ticker_info["marketCap"])
+        if ticker_info["marketCap"] > 999999
+        else humanize.intcomma(ticker_info["marketCap"]),
+        "open_price": humanize.intcomma(ticker_info["open"]),
     }
 
     return render_template(
-        'dashboard/info.html',
+        "dashboard/info.html",
         ticker_symbol=ticker_symbol,
         date_of_search=date_of_search,
         details=details,
         period_options=PERIOD_OPTIONS,
-        interval_options=INTERVAL_OPTIONS)
+        interval_options=INTERVAL_OPTIONS,
+    )
 
 
-@bp.route('/download', methods=('POST',))
+@bp.route("/download", methods=("POST",))
 def download():
-    ticker_symbol = request.form['ticker'].upper()
+    ticker_symbol = request.form["ticker"].upper()
 
-    if 'last_download_at' in session:
+    if "last_download_at" in session:
         seconds_since_last_download = int(
-            (datetime.now() - session['last_download_at']).total_seconds())
+            (datetime.now() - session["last_download_at"]).total_seconds()
+        )
         if seconds_since_last_download < DOWNLOAD_INTERVAL_SECONDS:
             flash(
-                f'Please wait {seconds_since_last_download} seconds '
-                f'before downloading again.')
-            return redirect(url_for('dashboard.info', ticker_symbol=ticker_symbol))
+                f"Please wait {seconds_since_last_download} seconds "
+                f"before downloading again."
+            )
+            return redirect(url_for("dashboard.info", ticker_symbol=ticker_symbol))
 
     yf_ticker = yf.Ticker(ticker_symbol)
-    period = request.form['period']
-    interval = request.form['interval']
+    period = request.form["period"]
+    interval = request.form["interval"]
     ticker_history = yf_ticker.history(period=period, interval=interval)
 
-    session['last_download_at'] = datetime.now()
+    session["last_download_at"] = datetime.now()
 
-    timestamp = datetime.now().strftime('%Y-%m-%d_%H%M%S')
-    filename = f'stock_data__{ticker_symbol}__{timestamp}.csv'
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H%M%S")
+    filename = f"stock_data__{ticker_symbol}__{timestamp}.csv"
     with tempfile.NamedTemporaryFile() as file:
         ticker_history.to_csv(file.name)  # write to tempfile in csv format
         return send_file(
-            file.name, mimetype='text/csv', as_attachment=True,
-            attachment_filename=filename)
+            file.name,
+            mimetype="text/csv",
+            as_attachment=True,
+            attachment_filename=filename,
+        )
