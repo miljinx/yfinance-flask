e498c8c200714ed0fe3a4951a823642481181916
Jim X. Lin <miljinx@gmail.com>
2020-08-30 00:04:37 -0700

    Add js linting

--- app/static/script.js
+++ app/static/script.js
@@ -1,31 +1,31 @@
-window.addEventListener('DOMContentLoaded', function(event) {
-  handleClickFind();
-  handleClickDownload();
-});
+window.addEventListener('DOMContentLoaded', function (event) {
+  handleClickFind()
+  handleClickDownload()
+})
 
-function handleClickFind() {
-  var form = document.getElementById("find-form");
-  if (form === null) return;
-  form.addEventListener("submit", function() {
-    var submitBtn = document.getElementById("find");
-    submitBtn.setAttribute("disabled", "");
-    submitBtn.setAttribute("value", "Finding");
-  });
+function handleClickFind () {
+  var form = document.getElementById('find-form')
+  if (form === null) return
+  form.addEventListener('submit', function () {
+    var submitBtn = document.getElementById('find')
+    submitBtn.setAttribute('disabled', '')
+    submitBtn.setAttribute('value', 'Finding...')
+  })
 }
 
 // TODO use fetch api for callback functionality
 // see https://blog.logrocket.com/programmatic-file-downloads-in-the-browser-9a5186298d5c/
-function handleClickDownload() {
-  var form = document.getElementById("download-form");
-  if (form === null) return;
-  var submitTimeout = 10;
-  form.addEventListener("submit", function() {
-    var submitBtn = document.getElementById("download");
-    submitBtn.setAttribute("disabled", "");
-    submitBtn.setAttribute("value", "Downloading...");
-    window.setTimeout(function() {
-      submitBtn.removeAttribute("disabled");
-      submitBtn.setAttribute("value", "Download");
-    }, 1000*submitTimeout);
-  });
+function handleClickDownload () {
+  var form = document.getElementById('download-form')
+  if (form === null) return
+  var submitTimeout = 10
+  form.addEventListener('submit', function () {
+    var submitBtn = document.getElementById('download')
+    submitBtn.setAttribute('disabled', '')
+    submitBtn.setAttribute('value', 'Downloading...')
+    window.setTimeout(function () {
+      submitBtn.removeAttribute('disabled')
+      submitBtn.setAttribute('value', 'Download')
+    }, 1000 * submitTimeout)
+  })
 }
